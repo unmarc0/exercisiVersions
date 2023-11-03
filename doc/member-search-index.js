76488d06643f43ff633ed27d9a397fb11d6b6d51
Marc Olive Morla <molive18294@iesjoanramis.org>
2023-11-03 19:11:12 +0100

    Final Commit

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"exercisiVersions","c":"exerciciVersions.Empleat","l":"Empleat(String, String, double)","u":"%3Cinit%3E(java.lang.String,java.lang.String,double)"},{"p":"exercisiVersions","c":"exerciciVersions","l":"exerciciVersions()","u":"%3Cinit%3E()"},{"p":"exercisiVersions","c":"exerciciVersions.Empleat","l":"pujadaSou(double)"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"exercisiVersions","l":"exerciciVersions.Empleat"},{"p":"exercisiVersions","l":"exerciciVersions"}];updateSearchResults();
